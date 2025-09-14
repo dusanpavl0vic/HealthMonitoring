232292330a97f3a9aecbfaf593f27476d9e32e77
Dusan Pavlovic <dusan.pavlovic016@gmail.com>
2025-09-15 00:59:00 +0200

    dodat template za gateway, dockercompose i docker file, i postresql

---
+++ gateway/src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
